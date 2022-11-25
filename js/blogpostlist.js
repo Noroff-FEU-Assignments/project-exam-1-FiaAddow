950545b60fe9e5106eaaefb831c71ed7caf5e1f3
FiaAddow <100525038+Fiaaddow1@users.noreply.github.com>
2022-11-25 02:05:03 +0100

    api fetch still wrong

--- js/blogpostlist.js
+++ js/blogpostlist.js
@@ -1,15 +1,35 @@
-const baseUrl = "https://www.edle.tech/wp-json/wp/v2/posts";
+
+const postsUrl = "https://www.edle.tech/wp-json/wp/v2/posts?pr-page";
+
 const postContainer = document.querySelector(".posts");
+const imageContainer = document.querySelector(".images");
 
-async function getPosts(url){
-    const response = await fetch(url);
-    const postsFromWP = await response.json();
+async function getPosts(url) {
+   try {
+      const response = await fetch(url);
+      const getWPPosts = await response.json();
 
-    console.log(postsFromWP);
+
+console.log(getWPPosts);
+
+getWPPosts.forEach(function(onePost){
+   console.log(onePost);
+   postContainer.innerHTML +=`
+   <div class="posts">
+   <h1>${onePost.slug}</h1>
+   <a href="blogpostlist.html?id= ${onePost.content}">
+    <div>
+      <img class="images" width="500" height="500" style="background-image:url('${onePost.embeddable}[0].thumbnail}')">
+    </div>
+   </div>`
+  });
+  }
+
+  catch (error) {
+   console.log(error);
+   postContainer.innerHTML = message("error", error);
+  }
 }
+   getPosts(postsUrl); 
 
-
-   getPosts(baseUrl); 
-
-
-   
+  
