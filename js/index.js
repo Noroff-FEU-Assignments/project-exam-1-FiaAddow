4dc690d71b9dfaf1adee4732b62ce73d38683032
FiaAddow <100525038+Fiaaddow1@users.noreply.github.com>
2022-11-28 17:57:23 +0100

    grid

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-var form = {};
-
-document.addEventListener("DOMContentLoaded", function() {
-    form.Name = document.getElementById('Name');
-   
-    form.email = document.getElementById('email');
-  
-    form.question = document.getElementById('question');
+let slider = tns({
+container: ".my-slider"
 })
-
-
-   function ifNotEmpty(value) {
-    if (value == null || typeof value == 'undefined' ) return false;
-    return (value.length > 0);
-   }
-//control if email is correct format
-   function isEmail(email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-   }
-
-   //sending form
-
-   function sendForm() {
-    form.Name = getName();
-
-    if (isValid()) {
-        let usr =new
-    }
