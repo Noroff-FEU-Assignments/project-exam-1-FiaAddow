a00b4c7d8d28d0c47006a340a610b4733d2652a5
FiaAddow <100525038+Fiaaddow1@users.noreply.github.com>
2022-12-03 22:04:26 +0100

    hotjar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const postsUrl = "https://edle.tech/wp-json/wp/v2/posts?_embed";
+
 const postsContainer = document.getElementById("posts-container");
 const post = document.querySelector(".post");
 const prevButton = document.getElementById("arrow-prev");
@@ -12,3 +14,27 @@ prevButton.addEventListener("click", () => {
   const postWidth = post.clientWidth;
   postsContainer.scrollLeft -= postWidth;
 });
+
+/*carusell*/
+
+async function getPosts(url) {
+  const response = await fetch(url);
+  const getWPPosts = await response.json();
+
+
+
+
+getWPPosts.forEach(function(onePost){
+console.log(onePost._embedded['wp:featuredmedia'][0].source_url);
+postsContainer.innerHTML +=`
+<li class="post">
+    <img src="${onePost._embedded['wp:featuredmedia'][0].source_url}">
+    </li>`
+});
+
+
+
+}
+getPosts(postsUrl); 
+
+
