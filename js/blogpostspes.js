ed3d055d452af9081473e1850c0cdf3dbddb9887
FiaAddow <100525038+Fiaaddow1@users.noreply.github.com>
2022-12-03 21:20:52 +0100

    blogpost spes page api done

--- js/blogpostspes.js
+++ js/blogpostspes.js
@@ -1,22 +1,22 @@
-const postUrl = "https://edle.tech/wp-json/wp/v2/posts?_";
+const postUrl = "https://edle.tech/wp-json/wp/v2/posts/";
 const postContainer = document.querySelector(".onepost");
+
 
 const queryString = document.location.search;
 
 const params = new URLSearchParams(queryString);
 
-const postContent = params.get("content");
-
+const postContent = params.get("id");
 
 async function getPost(url){
     const newUrl = url + postContent
-
+console.log(newUrl)
     const response = await fetch(newUrl);
-    const postContent = await response.json();
-console.log(onePost.content);
+    const onePost = await response.json();
+console.log(onePost);
        postContainer.innerHTML += `
         <div class="onepost">
-          <h1>${content.rendered}</h1>
+          ${onePost.content.rendered}
         </div>
         `
         
